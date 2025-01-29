caa0fecf3ed24fa073eb8f091f9c78f8a6655041
freddyB19 <freddyybolivar2013@gmail.com>
2025-01-29 15:55:52 -0400

    add: implementación de la app

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import os
+import sys
+BASE = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE)
+sys.path.append(os.path.join(BASE, "apps"))
 
+from fastapi import FastAPI
+from apps.users.routes import router as router_users
 
+app = FastAPI()
+app.include_router(router_users)
