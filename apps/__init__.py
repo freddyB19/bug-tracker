5eb3be8dc3590db08ce266aae5136c7ea00d2706
freddyB19 <freddyybolivar2013@gmail.com>
2025-02-13 17:06:10 -0400

    feat: add variables de entorno

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,8 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
+load_dotenv()
+
 DB_URL = "sqlite:///./bug_tracker.db"
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM_JWT = os.getenv('ALGORITHM_JWT')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
+REFRESH_TOKEN_EXPIRE_MINUTES = int(os.getenv('REFRESH_TOKEN_EXPIRE_MINUTES'))
 
 engine = create_engine(
 	DB_URL
