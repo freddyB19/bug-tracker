7292b5681864aef196e2b63d1eeee4d4d76a1b5d
freddyB19 <freddyybolivar2013@gmail.com>
2025-02-20 13:14:12 -0400

    feat: add funciones para usar tokens

---
+++ apps/utils/token/exceptions.py
@@ -0,0 +1,37 @@
+import jwt
+
+
+GENERIC_ERROR_MESSAGE = 'token error'
+
+
+class TokenExpiredError(Exception):
+
+	def __init__(self, message:str = "El token a expirado") -> None:
+		self.message = message
+
+		super().__init__(self.message)
+
+
+	def __str__(self):
+		return f"Token invalido: {self.message}"
+
+
+class TokenInvalidError(Exception):
+
+	def __init__(self, message: str = "Firma del token no valida") -> None:
+		self.message = message
+		super().__init__(self.message)
+
+	def __str__(self):
+		return f"Firma invalida: {self.message}"
+
+
+class TokenImmatureError(Exception):
+
+	def __init__(self, message: str = "Error en la estimación del tiempo del token") -> None:
+		self.message = message
+
+		super().__init__(self.message)
+
+	def __str__(self):
+		return f"Error en el Token: {self.message}"
---
+++ apps/utils/token/token.py
@@ -0,0 +1,72 @@
+import datetime
+from typing import Dict
+
+
+import jwt
+
+from apps import (
+	SECRET_KEY,
+	ALGORITHM_JWT,
+	ACCESS_TOKEN_EXPIRE_HOURS,
+	REFRESH_TOKEN_EXPIRE_HOURS
+)
+from apps.utils.token.exceptions import (
+	TokenExpiredError,
+	TokenInvalidError,
+	TokenImmatureError
+) 
+
+def validate_token(token):
+	try:
+		jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM_JWT])
+	except jwt.ExpiredSignatureError:
+		raise TokenExpiredError()
+
+	except  jwt.InvalidSignatureError:
+		raise TokenInvalidError()
+
+	except jwt.ImmatureSignatureError:
+		raise TokenImmatureError()
+
+
+def verify_token(token: str = ""):
+	message = ""
+	
+	try:
+		validate_token(token)
+		return {"state": True, "message": "OK"}
+	except TokenExpiredError as e:
+		message = e
+	except TokenInvalidError as e:
+		message = e
+	except TokenImmatureError as e:
+		message = e
+
+	return {"state": False, "message": message}
+
+
+def create_token(infoDict: Dict):
+	data = infoDict.copy()
+
+	data.update({
+		"exp": datetime.datetime.utcnow() + datetime.timedelta(hours = ACCESS_TOKEN_EXPIRE_HOURS),
+		"iat": datetime.datetime.utcnow()
+		#  identifica el momento en el que se emitió el JWT.
+	})
+
+	token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM_JWT)
+
+	return token
+
+
+def create_refresh_token(infoDict: Dict):
+	data = infoDict.copy()
+
+	data.update({
+		"exp": datetime.datetime.utcnow() + datetime.timedelta(hours = REFRESH_TOKEN_EXPIRE_HOURS),
+		"iat": datetime.datetime.utcnow()
+	})
+
+	token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM_JWT)
+
+	return token
