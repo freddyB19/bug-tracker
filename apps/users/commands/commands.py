9e60ec951a196f4190b93d736bcfe1818385b355
freddyB19 <freddyybolivar2013@gmail.com>
2025-02-20 17:33:52 -0400

    rebuild: removiendo funciones de hash password de user/commands

--- apps/users/commands/commands.py
+++ apps/users/commands/commands.py
@@ -3,69 +3,11 @@ from typing import List
 from typing import Dict
 from typing import Optional
 
-
-
-from abc import ABC
-from abc import abstractmethod
-from typing_extensions import Annotated
-
-import bcrypt
-from pydantic import Field
 from pydantic import validate_call
-from pydantic import ValidationError
-from pydantic import PlainSerializer
-from pydantic.type_adapter import TypeAdapter
-
 
 from apps.users.models import User
-
-
-TypePassword = Annotated[
-	str, 
-	Field(frozen=True, strict=True),
-	PlainSerializer(lambda value: value.encode('utf-8'), return_type=bytes)
-]
-
-TypePasswordHashed = Annotated[
-	bytes, 
-	Field(frozen=True, strict=True)
-]
-
-
-hashPassword = TypeAdapter(TypePassword)
-
-class Hashed(ABC):
-	
-	@classmethod
-	@abstractmethod
-	@validate_call
-	def getHash(password: str) -> bytes:
-		pass
-
-class ValidateHash(ABC):
-
-	@classmethod
-	@abstractmethod
-	@validate_call
-	def is_validate(password: TypePasswordHashed, passwordHashed: TypePasswordHashed) -> bool:
-		pass
-
-
-class HashPassword(Hashed):
-	def getHash(password: str) -> bytes:
-		passwordBytes = hashPassword.dump_python(password)
-
-		try:
-			return bcrypt.hashpw(passwordBytes, bcrypt.gensalt(10))
-		except ValidationError as e:
-			raise ValueError("Error en el password, el tipo de dato ingresado no es valido")
-		
-
-class ValidateHashedPassword(ValidateHash):
-	def is_validate(passwordPlainText: str, passwordHashed: TypePasswordHashed) -> bool:
-		passwordBytes = hashPassword.dump_python(passwordPlainText)
-		
-		return bcrypt.checkpw(passwordBytes, passwordHashed)
+from apps.users.commands.utils.password import HashPassword
+from apps.users.commands.utils.password import ValidateHashedPassword
 
 
 @validate_call
@@ -154,7 +96,7 @@ def command_update_user(id, userInfo, db: List[Dict] | None = None) -> tuple:
 	return (newTable, user)
 
 @validate_call
-def command_login(user, db:List[Dict] | None = None) -> Dict[str, int | str]:
+def command_login(user, db:List[Dict] | None = None) -> Dict[str, int | str]: #User
 	data = [userInfo for userInfo in db if userInfo["email"] == user.email]
 
 	if not data:
