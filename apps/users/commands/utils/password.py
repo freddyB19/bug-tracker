51ca505cf0a0e6207eb75456e5a2348934189b42
freddyB19 <freddyybolivar2013@gmail.com>
2025-03-02 18:12:45 -0400

    update: add cambios en user_properties_serializer

--- apps/users/commands/utils/password.py
+++ apps/users/commands/utils/password.py
@@ -8,8 +8,11 @@ import bcrypt
 from pydantic import Field
 from pydantic import validate_call
 from pydantic import PlainSerializer
+from pydantic import ValidationError
 from pydantic.type_adapter import TypeAdapter
 
+from apps.users.models import User
+from apps.users.schemas import schemas
 
 TypePassword = Annotated[
 	str, 
@@ -66,7 +69,20 @@ class ValidateHashedPassword(ValidateHash):
 		return bcrypt.checkpw(passwordBytes, passwordHashed)
 
 
-def user_properties_serializer(user: Dict) -> Dict:
-	str_password = decodePassword.dump_python(user['password'])
-	user['password'] = str_password
-	return user
+def user_properties_serializer(user: User) -> Dict[str, str | int]:
+	try:
+		serializer = schemas.UserResponse(
+			id = user.id,
+			name = user.name,
+			email = user.email,
+			username = user.username,
+		)
+	except ValidationError as e:
+		return None
+
+	userDict = serializer.model_dump()
+	userDict.update({
+		"password": decodePassword.dump_python(user.password)
+	})
+
+	return userDict
