ddc1b914a800da5bdcfbcbddbffdc48ebc15b9b0
freddyB19 <freddyybolivar2013@gmail.com>
2025-03-02 18:17:18 -0400

    rebuild: moviendo user_properties_serializer a utils.py

--- apps/users/commands/utils/password.py
+++ apps/users/commands/utils/password.py
@@ -67,22 +67,3 @@ class ValidateHashedPassword(ValidateHash):
 		passwordBytes = hashPassword.dump_python(passwordPlainText)
 		
 		return bcrypt.checkpw(passwordBytes, passwordHashed)
-
-
-def user_properties_serializer(user: User) -> Dict[str, str | int]:
-	try:
-		serializer = schemas.UserResponse(
-			id = user.id,
-			name = user.name,
-			email = user.email,
-			username = user.username,
-		)
-	except ValidationError as e:
-		return None
-
-	userDict = serializer.model_dump()
-	userDict.update({
-		"password": decodePassword.dump_python(user.password)
-	})
-
-	return userDict
