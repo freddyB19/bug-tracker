a9d2c2e9c963cfd60416e6570a209ed77210b9f7
freddyB19 <freddyybolivar2013@gmail.com>
2025-03-09 22:58:07 -0400

    feat: add get_project

--- apps/projects/routes/routes.py
+++ apps/projects/routes/routes.py
@@ -23,3 +23,20 @@ def create_project(project: schemas.ProjectRequest) -> schemas.ProjectResponse:
 		)
 
 	return project
+
+
+@router.get(
+	"/{id}",
+ 	status_code = status.HTTP_200_OK,
+ 	response_model = schemas.ProjectResponse,
+ )
+def get_project(id: int) -> schemas.ProjectResponse:
+	try:
+		project = commands.command_get_project(project_id = id)
+	except ValueError as e:
+		return JSONResponse(
+			content = {"message": str(e)},
+			status_code = status.HTTP_404_NOT_FOUND
+		)
+
+	return project
