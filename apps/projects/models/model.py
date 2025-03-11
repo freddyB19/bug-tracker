870d9462f44a80c1f8639fe3d17f89ffeb973859
freddyB19 <freddyybolivar2013@gmail.com>
2025-03-11 18:22:59 -0400

    feat: add table Ticket

--- apps/projects/models/model.py
+++ apps/projects/models/model.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import enum
 from datetime import datetime
 from sqlalchemy import Enum
@@ -17,6 +19,17 @@ class ChoicesPrority(enum.Enum):
 	alta = 2
 	inmediata = 3
 
+class ChoicesState(enum.Enum):
+	nuevo = 0
+	desarrollo = 1
+	pruba = 2
+	revisión = 3
+	terminado = 4
+
+class ChoicesType(enum.Enum):
+	abierto = 0
+	archivado = 1
+	cerrado = 2
 
 class Project(Model):
 	__tablename__ = "project"
@@ -36,8 +49,48 @@ class Project(Model):
 	)
 
 	user: Mapped['User'] = relationship(back_populates = "projects")
+	tickets: Mapped[List['Ticket']] = relationship(back_populates = "project")
+
 
 	def __repr__(self):
 		return f"Project(id={self.id}, title={self.title}, priority={self.priority})"
+
+
+class Ticket(Model):
+	__tablename__ = "ticket"
+	
+	id: Mapped[int] = mapped_column(primary_key = True, index = True, nullable=False, unique=True)
+	title: Mapped[str] = mapped_column(String(30))
+	description: Mapped[str | None] = mapped_column(String(200))
+	priority: Mapped[Enum] = mapped_column(
+		Enum(ChoicesPrority), 
+		insert_default=ChoicesPrority.baja
+	)
+	created: Mapped[datetime] = mapped_column(insert_default = func.now())
+	updated: Mapped[datetime] = mapped_column(insert_default = func.now(), onupdate=func.now())
+	state: Mapped[Enum] = mapped_column(
+		Enum(ChoicesState), 
+		insert_default=ChoicesState.nuevo
+	)
+	type: Mapped[Enum] = mapped_column(
+		Enum(ChoicesType), 
+		insert_default=ChoicesType.abierto
+	)
+	project_id: Mapped[int] = mapped_column(
+		ForeignKey(
+			'project.id',
+			ondelete = "CASCADE",
+			onupdate = "CASCADE"
+		)
+	)
+
+	project: Mapped[Project] = relationship(back_populates = "tickets")
+
+	def __repr__(self):
+		return f"Ticket(id={self.id}, title={self.title}, priority={self.priority}, state={self.state}, type={self.type})"
+
+
+
+
 
 
