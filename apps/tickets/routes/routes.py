eba40c7641eeb1550903cefcafab1c8798090a0a
freddyB19 <freddyybolivar2013@gmail.com>
2025-03-18 22:20:00 -0400

    feat: add route get_ticket_by_filter

--- apps/tickets/routes/routes.py
+++ apps/tickets/routes/routes.py
@@ -1,5 +1,8 @@
+from typing_extensions import Annotated
+
 from fastapi import status
 from fastapi import APIRouter
+from fastapi import Query
 from fastapi.responses import JSONResponse
 
 
@@ -43,3 +46,25 @@ def get_ticket(id: int) -> schemas.TicketResponse:
 		)
 
 	return ticket
+
+@router.get(
+	"/search/",
+	status_code = status.HTTP_200_OK,
+	response_model = schemas.ListTicketsResponse
+)
+def get_ticket_by_filter(ticket_filter: Annotated[schemas.TicketFilter, Query()]) -> schemas.ListTicketsResponse:
+	try:
+		tickets = commands.command_get_ticket_by_filter(
+			ticket = ticket_filter
+		)
+	except ValueError as e:
+		return JSONResponse(
+			content = {"message": str(e)},
+			status_code = status.HTTP_404_NOT_FOUND
+		)
+
+	response = {
+		"tickets": tickets
+	}
+	
+	return response
