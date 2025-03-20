971e9abe6172a668065b038cd7a1a679b111f1e6
freddyB19 <freddyybolivar2013@gmail.com>
2025-03-19 21:58:59 -0400

    feat: add validation token in endpoints-tickets

--- apps/tickets/routes/routes.py
+++ apps/tickets/routes/routes.py
@@ -4,10 +4,13 @@ from typing_extensions import Annotated
 from fastapi import status
 from fastapi import APIRouter
 from fastapi import Query
+from fastapi import Depends
 from fastapi.responses import JSONResponse
 
 from apps.tickets.schemas import schemas
 from apps.tickets.commands import commands
+from apps.utils.token.token import validate_authorization
+
 
 router = APIRouter(prefix = "/ticket")
 
@@ -16,7 +19,7 @@ router = APIRouter(prefix = "/ticket")
 	status_code = status.HTTP_201_CREATED,
 	response_model = schemas.TicketResponse
 )
-def create_ticket(ticket: schemas.TicketRequest) -> schemas.TicketResponse:
+def create_ticket(ticket: schemas.TicketRequest, token: str = Depends(validate_authorization)) -> schemas.TicketResponse:
 	try:
 		new_ticket = commands.command_create_ticket(
 			ticket = ticket
@@ -35,7 +38,7 @@ def create_ticket(ticket: schemas.TicketRequest) -> schemas.TicketResponse:
 	status_code = status.HTTP_200_OK,
 	response_model = schemas.TicketResponse
 )
-def get_ticket(id: int) -> schemas.TicketResponse:
+def get_ticket(id: int, token: str = Depends(validate_authorization)) -> schemas.TicketResponse:
 	try:
 		ticket = commands.command_get_ticket(
 			ticket_id = id
@@ -54,7 +57,7 @@ def get_ticket(id: int) -> schemas.TicketResponse:
 	status_code = status.HTTP_200_OK,
 	response_model = schemas.ListTicketsResponse
 )
-def get_ticket_by_filter(ticket_filter: Annotated[schemas.TicketFilter, Query()]) -> schemas.ListTicketsResponse:
+def get_ticket_by_filter(ticket_filter: Annotated[schemas.TicketFilter, Query()], token: str = Depends(validate_authorization)) -> schemas.ListTicketsResponse:
 	try:
 		tickets = commands.command_get_ticket_by_filter(
 			ticket = ticket_filter
@@ -77,7 +80,7 @@ def get_ticket_by_filter(ticket_filter: Annotated[schemas.TicketFilter, Query()]
 	status_code = status.HTTP_200_OK,
 	response_model = schemas.ListTicketsResponse
 )
-def get_ticket_by_title(ticket: schemas.TicketByTitle) ->  schemas.ListTicketsResponse:
+def get_ticket_by_title(ticket: schemas.TicketByTitle, token: str = Depends(validate_authorization)) ->  schemas.ListTicketsResponse:
 	try:
 		tickets = commands.command_get_ticket_by_title(
 			ticket = ticket
@@ -99,7 +102,7 @@ def get_ticket_by_title(ticket: schemas.TicketByTitle) ->  schemas.ListTicketsRe
 	status_code = status.HTTP_200_OK,
 	response_model = schemas.TicketSimpleResponse
 )
-def update_ticket(id: int, ticket: schemas.TicketUpdate) -> schemas.TicketSimpleResponse:
+def update_ticket(id: int, ticket: schemas.TicketUpdate, token: str = Depends(validate_authorization)) -> schemas.TicketSimpleResponse:
 	try:
 		ticket = commands.command_update_ticket(
 			ticket_id = id,
@@ -118,7 +121,7 @@ def update_ticket(id: int, ticket: schemas.TicketUpdate) -> schemas.TicketSimple
 	"/{id}",
 	status_code = status.HTTP_204_NO_CONTENT,
 )
-def delete_ticket(id: int) -> None:
+def delete_ticket(id: int, token: str = Depends(validate_authorization)) -> None:
 	try:
 		commands.command_delete_ticket(
 			ticket_id = id
