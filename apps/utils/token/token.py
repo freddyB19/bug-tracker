c9574b0c2308192e8aefe9594089df158ea95eda
freddyB19 <freddyybolivar2013@gmail.com>
2025-03-24 00:08:12 -0400

    feat: add validations in utils/token

--- apps/utils/token/token.py
+++ apps/utils/token/token.py
@@ -1,5 +1,8 @@
 import datetime
+from typing import Any
 from typing import Dict
+from typing import Optional
+
 
 from typing_extensions import Annotated
 
@@ -9,7 +12,11 @@ from fastapi import status
 from fastapi import Header
 from fastapi import HTTPException
 
+from pydantic import BaseModel
 from pydantic import validate_call
+from pydantic import ConfigDict
+from pydantic import ValidationError
+from pydantic import AfterValidator
 
 from apps import (
 	SECRET_KEY,
@@ -23,7 +30,30 @@ from apps.utils.token.exceptions import (
 	TokenImmatureError
 ) 
 
-def validate_token(token):
+
+class DecodeTokenResult(BaseModel):
+	data: Optional[Dict[str, Any]] = {}
+	message: Optional[str] = "OK"
+	state: Optional[bool] = True
+
+
+class VerifyTokenResult(BaseModel):
+	message: str = "OK"
+	state: bool = True
+
+
+def is_empty(value: Dict[str, Any]) ->  Dict[str, Any]:
+	if not value:
+		raise ValueError("El diccionario esta vacio")
+
+	return value
+
+isEmpty = Annotated[Dict[str, Any], AfterValidator(is_empty)]
+
+
+@validate_call(config = ConfigDict(hide_input_in_errors=True))
+def validate_token(token: str) -> None:
+
 	try:
 		jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM_JWT])
 	except jwt.ExpiredSignatureError:
@@ -36,12 +66,14 @@ def validate_token(token):
 		raise TokenImmatureError()
 
 
-def verify_token(token: str = ""):
+@validate_call(config = ConfigDict(hide_input_in_errors=True))
+def verify_token(token: str = "") -> VerifyTokenResult:
+	
 	message = ""
 	
 	try:
 		validate_token(token)
-		return {"state": True, "message": "OK"}
+		return VerifyTokenResult(state = True, message = "OK")
 	except TokenExpiredError as e:
 		message = e
 	except TokenInvalidError as e:
@@ -49,10 +81,15 @@ def verify_token(token: str = ""):
 	except TokenImmatureError as e:
 		message = e
 
-	return {"state": False, "message": message}
+	return VerifyTokenResult(state = False, message = str(message))
 
 
-def create_token(infoDict: Dict):
+@validate_call(config = ConfigDict(hide_input_in_errors=True))
+def create_token(infoDict: isEmpty) -> str:
+
+	if not infoDict:
+		raise ValidationError("")
+	
 	data = infoDict.copy()
 
 	data.update({
@@ -65,8 +102,9 @@ def create_token(infoDict: Dict):
 
 	return token
 
-
-def create_refresh_token(infoDict: Dict):
+@validate_call(config = ConfigDict(hide_input_in_errors=True))
+def create_refresh_token(infoDict: isEmpty) -> str:
+	
 	data = infoDict.copy()
 
 	data.update({
@@ -78,16 +116,32 @@ def create_refresh_token(infoDict: Dict):
 
 	return token
 
-def decode_token(token: str) -> Dict[str, str | int]:
-	return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM_JWT])
 
-@validate_call
+@validate_call(config = ConfigDict(hide_input_in_errors=True))
+def decode_token(token: str) -> DecodeTokenResult:
+
+	message = "" 
+	
+	try:
+		data = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM_JWT])
+		return DecodeTokenResult(data = data)
+	
+	except jwt.ExpiredSignatureError:
+		message = "El token a expirado"
+
+	except  jwt.InvalidSignatureError:
+		message = "Firma del token no valida" 
+
+	except jwt.ImmatureSignatureError:
+		message = "Error en la estimación del tiempo del token"
+
+	return DecodeTokenResult(message = message, state = False)
+
+
+@validate_call(config = ConfigDict(hide_input_in_errors=True))
 def extract_token_from_str(auth: str) -> str:
 	find = "Bearer "
 	
-	if not isinstance(auth, str):
-		raise ValueError("El dato recibido no es una cadena de texto")
-
 	if not auth.startswith(find):
 		raise ValueError("Error en el formato de envio del token")
 	
@@ -105,7 +159,7 @@ def validate_authorization(authorization: Annotated[str | None, Header()] = None
 	
 	result = verify_token(token = token)
 
-	if not result['state']:
+	if not result.state:
 		raise HTTPException(
 				status_code = status.HTTP_401_UNAUTHORIZED, 
 				detail = "Token no valido",
