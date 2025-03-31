fda5ca62293da6ecaa238e9c402429fdf4e807ba
freddyB19 <freddyybolivar2013@gmail.com>
2025-03-31 18:11:20 -0400

    style-feat: change name and add validation in param

--- apps/users/commands/utils/utils.py
+++ apps/users/commands/utils/utils.py
@@ -1,14 +1,36 @@
+from typing import Any
 from typing import Dict
+from typing import Annotated
 
+from pydantic import BaseModel
+from pydantic import ConfigDict
 from pydantic import ValidationError
+from pydantic import BeforeValidator
+from pydantic import validate_call
 
 from apps.users.models import User
-from apps.users.schemas import schemas
 from .password import decodePassword
 
-def user_properties_serializer(user: User) -> Dict[str, str | int]:
+def is_validate(value: User) -> User:
+	if not isinstance(value, User):
+		raise ValueError("El objeto no es del tipo 'User'")
+
+	return value
+
+TypeData = Annotated[Any, User]
+UserType = Annotated[TypeData, BeforeValidator(is_validate)]
+
+
+class UserSerializer(BaseModel):
+	id: int
+	name: str
+	email: str
+	username: str
+
+@validate_call(config = ConfigDict(hide_input_in_errors = True))
+def user_serializer(user: UserType) -> Dict[str, str | int]:
 	try:
-		serializer = schemas.UserResponse(
+		serializer = UserSerializer(
 			id = user.id,
 			name = user.name,
 			email = user.email,
