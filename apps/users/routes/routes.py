6023fb8b2537876208d8838e91ea0d1f6b20c8a0
freddyB19 <freddyybolivar2013@gmail.com>
2025-04-09 18:46:17 -0400

    feat: add handling errors code and messages in routes

--- apps/users/routes/routes.py
+++ apps/users/routes/routes.py
@@ -12,6 +12,12 @@ from apps.utils.token.token import validate_authorization
 
 router = APIRouter(prefix = "/user")
 
+STATUS_CODE_ERRORS = {
+	400: status.HTTP_400_BAD_REQUEST,
+	401: status.HTTP_401_UNAUTHORIZED,
+	404: status.HTTP_404_NOT_FOUND,
+	409: status.HTTP_409_CONFLICT,
+}
 
 @router.post(
 	"/", 
@@ -22,9 +28,11 @@ def create_user(user: schemas.UserRequest) -> schemas.UserResponse:
 	try:
 		new_user = commands.command_create_user(user = user)
 	except ValueError as e:
+		message, status_code = e.args
+
 		return JSONResponse(
-			content = {"message": str(e)},
-			status_code = status.HTTP_400_BAD_REQUEST
+			content = {"message": message},
+			status_code = STATUS_CODE_ERRORS[status_code]
 		)
 		
 	return new_user
@@ -38,9 +46,11 @@ def get_user(id: int, token: str = Depends(validate_authorization)) -> schemas.U
 	try:
 		user = commands.command_get_user(user_id = id)
 	except ValueError as e:
+		message, status_code = e.args
+		
 		return JSONResponse(
-			content = {"message": str(e)},
-			status_code = status.HTTP_404_NOT_FOUND
+			content = {"message": message},
+			status_code = STATUS_CODE_ERRORS[status_code]
 		)
 
 	return user
@@ -52,9 +62,11 @@ def delete_user(id: int, token: str = Depends(validate_authorization)) -> Respon
 	try:
 		commands.command_delete_user(user_id = id)
 	except ValueError as e:
+		message, status_code = e.args
+		
 		return JSONResponse(
-			content = {"message": str(e)},
-			status_code = status.HTTP_404_NOT_FOUND
+			content = {"message": message},
+			status_code = STATUS_CODE_ERRORS[status_code]
 		)
 
 	return JSONResponse(
@@ -74,9 +86,11 @@ def update_email(id: int, user: schemas.UserEmail, token: str = Depends(validate
 	try:
 		user = commands.command_update_email_user(user_id = id, infoUpdate = user) 
 	except ValueError as e:
+		message, status_code = e.args
+		
 		return JSONResponse(
-			content = {"message": str(e)},
-			status_code = status.HTTP_404_NOT_FOUND
+			content = {"message": message},
+			status_code = STATUS_CODE_ERRORS[status_code]
 		)
 
 	return user
@@ -91,9 +105,11 @@ def update_password(id: int, user: schemas.UserPassword, token: str = Depends(va
 	try:
 		user = commands.command_update_password_user(user_id = id, infoUpdate = user)
 	except ValueError as e:
+		message, status_code = e.args
+		
 		return JSONResponse(
-			content = {"message": str(e)},
-			status_code = status.HTTP_404_NOT_FOUND
+			content = {"message": message},
+			status_code = STATUS_CODE_ERRORS[status_code]
 		)
 
 	return user
@@ -108,9 +124,11 @@ def update_username(id: int, user: schemas.UserUsername,token: str = Depends(val
 	try:
 		user = commands.command_update_username_user(user_id = id, infoUpdate = user)
 	except ValueError as e:
+		message, status_code = e.args
+		
 		return JSONResponse(
-			content = {"message": str(e)},
-			status_code = status.HTTP_404_NOT_FOUND
+			content = {"message": message},
+			status_code = STATUS_CODE_ERRORS[status_code]
 		)
 
 	return user
@@ -122,10 +140,12 @@ def update_username(id: int, user: schemas.UserUsername,token: str = Depends(val
 def login(user: schemas.UserLogin) -> schemas.UserLoginResponse:
 	try:
 		result = commands.command_login(infoLogin = user)
-	except Exception as e:
+	except ValueError as e:
+		message, status_code = e.args
+
 		return JSONResponse(
-			content = {"message": str(e)},
-			status_code = status.HTTP_400_BAD_REQUEST
+			content = {"message": message},
+			status_code = STATUS_CODE_ERRORS[status_code]
 		)
 
 	return result
