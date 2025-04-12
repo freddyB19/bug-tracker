2c9cd6c026064533fe2b75fae9d1f611ff4e2f6a
freddyB19 <freddyybolivar2013@gmail.com>
2025-04-12 18:14:50 -0400

    feat: add errors messages for 'projects'

---
+++ apps/projects/commands/utils/error_messages.py
@@ -0,0 +1,24 @@
+from typing import List
+from pydantic import validate_call
+from pydantic import ConfigDict
+
+
+class InvalidPriority:
+	@classmethod
+	@validate_call(config = ConfigDict(hide_input_in_errors = True))
+	def get(cls, choices:List[str]) -> str:
+		return f"La prioridad indicada es incorrecta, debe ser entre {choices}"
+
+
+class DoesNotExistsProject:
+	@classmethod
+	@validate_call(config = ConfigDict(hide_input_in_errors = True))
+	def get(cls, id: int) -> str:
+		return f"No existe información sobre el proyecto con ID: {id}"
+
+
+class UnauthorizedProject:
+	@classmethod
+	@validate_call(config = ConfigDict(hide_input_in_errors = True))
+	def get(cls, id: int) -> str:
+		return f"No existe información sobre este proyecto o no le pertenece a este usuario. Proyecto ID: '{id}'"
