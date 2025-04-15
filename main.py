e597013a4fba8a990bb41029b1cb0660d8ccfa3d
freddyB19 <freddyybolivar2013@gmail.com>
2025-04-15 00:02:45 -0400

    feat: add exception handler in app

--- main.py
+++ main.py
@@ -4,17 +4,35 @@ BASE = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE)
 sys.path.append(os.path.join(BASE, "apps"))
 
+from fastapi import status
 from fastapi import FastAPI
+from fastapi import Request
+from fastapi.exceptions import RequestValidationError
+
+from fastapi.responses import JSONResponse
+
+from pydantic import ValidationError
+
+from apps import Model
+from apps import engine
+
 from apps.users.routes import router as router_users
 from apps.tickets.routes import router as router_ticket
 from apps.projects.routes import router as router_project
 
-
-from apps import engine
-from apps import Model
-
-
 app = FastAPI()
+
+@app.exception_handler(RequestValidationError)
+def validation_error_exception_handler(request: Request, exc: RequestValidationError):
+	error = exc.errors()[0]
+
+	message = error['msg'].replace("Value error,", "")
+	detail = f"Input error: {error['input']}" 
+	
+	return JSONResponse(
+		 status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+		 content = {"messge": f"{message}", "detail": detail}
+	)
 
 Model.metadata.create_all(engine)
 
