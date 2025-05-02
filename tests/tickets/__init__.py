b093780926d375ebed83cd5fddf14941ef690c18
freddyB19 <freddyybolivar2013@gmail.com>
2025-05-01 20:26:32 -0400

    feat: add bulk_insert_tickets for tests/tickets

--- tests/tickets/__init__.py
+++ tests/tickets/__init__.py
@@ -1,3 +1,9 @@
+from typing import Literal
+
+from sqlalchemy import insert
+
+from pydantic import BaseModel
+
 from apps.tickets.models import Ticket
 from apps.tickets.models import ChoicesType
 from apps.tickets.models import ChoicesState
@@ -40,3 +46,78 @@ def set_ticket_schema(title:str = None, description:str = None, priority:str = N
 		priority = set_priority,
 		project_id = set_project_id
 	)
+
+
+class CreateTicket(BaseModel):
+	title: str
+	priority: Literal["baja", "normal", "alta", "inmediata"] = "normal"
+	state: Literal["nuevo", "desarrollo", "prueba", "repaso", "terminado"] = "nuevo"
+	type: Literal["abierto", "archivado", "cerrado"] = "abierto"
+	description: str | None = None
+	project_id: int = 1
+
+
+def bulk_insert_ticket(db):
+	db.execute(
+	    insert(Ticket),
+	    [
+	        CreateTicket(
+	        	title = "Interfaz Grafica"
+	        ).model_dump(),
+	        CreateTicket(
+	        	title = "Formularios"
+	        ).model_dump(),
+	        CreateTicket(
+	        	title = "Confirmación",
+	        	priority = "inmediata"
+	        ).model_dump(),
+	        CreateTicket(
+	        	title = "Tests",
+	        	priority = "inmediata"
+	        ).model_dump(),
+	        CreateTicket(
+	        	title = "Endpoints",
+	        	priority = "alta",
+	        	state = "desarrollo"
+	        ).model_dump(),
+	        CreateTicket(
+	        	title = "Limites de petición",
+	        	priority = "alta",
+	        	state = "desarrollo"
+	        ).model_dump(),
+	        CreateTicket(
+	        	title = "Refactoring",
+	        	state = "repaso"
+	        ).model_dump(),
+	        CreateTicket(
+	        	title = "Validaciones de Login",
+	        	priority = "alta",
+	        	state = "repaso"
+	        ).model_dump(),
+	        CreateTicket(
+	        	title = "Consultas SQL",
+	        	state = "repaso"
+	        ).model_dump(),
+	        CreateTicket(
+	        	title = "Rendimiento de la API",
+	        	priority = "baja",
+	        	state = "repaso"
+	        ).model_dump(),
+	        CreateTicket(
+	        	title = "Tabla Ticket",
+	        	priority = "baja",
+	        	type = "archivado"
+	        ).model_dump(),
+	        CreateTicket(
+	        	title = "Algoritmo de busqueda",
+	        	priority = "baja",
+	        	type = "archivado"
+	        ).model_dump(),
+	        CreateTicket(
+	        	title = "Sistema de caching",
+	        	type = "cerrado"
+	        ).model_dump()
+	    ]
+	)
+
+	db.commit()
