4bddf5cd135ccf9b6c47e4920691ee16468b1e83
freddyB19 <freddyybolivar2013@gmail.com>
2025-05-02 21:31:06 -0400

    feat: add default values and calculate function for pagination

--- apps/utils/pagination/pagination.py
+++ apps/utils/pagination/pagination.py
@@ -1,3 +1,5 @@
+import math
+
 from typing import Any
 from typing import List
 from typing import Dict
@@ -15,9 +17,16 @@ from fastapi import Request
 
 ListElements = Optional[List[Any]]
 
+PageDefault = 1
+PageLowerLimit = 1
+PageSizeDefault = 10
+PageSizeUpperLimit = 20
+PageSizeLowerLimit = 1
+
+
 class ListPagination(BaseModel):
-	page: int = Field(ge = 0, default = 0)
-	pageSize: int = Field(ge = 1, le = 20, default = 10) #10
+	page: int = Field(ge = PageLowerLimit, default = PageDefault)
+	pageSize: int = Field(ge = PageSizeLowerLimit, le = PageSizeUpperLimit, default = PageSizeDefault) #10
 
 
 class ResponsePagination(BaseModel):
@@ -53,11 +62,14 @@ def set_url_pagination(request: RequestContext, elements: ListElements, total_el
 	url_next = f"{url_base}?page={page + 1}&pageSize={pageSize}{url_params['url']}"
 	url_previous = f"{url_base}?page={page - 1}&pageSize={pageSize}{url_params['url']}"
 
-	elements_per_page = page + pageSize
+	pages = math.ceil(total_elements / pageSize)
+	
+	has_previous = url_previous if (page > 1) and elements else None
+	has_next = url_next if page < pages else None
 
 	return {
-		"previous": url_previous if (page >= 1 ) and elements else None,
-		"next": url_next if (elements_per_page < total_elements) and elements else None,
+		"previous": has_previous,
+		"next": has_next
 	}
 
 
@@ -69,3 +81,15 @@ def get_url_from_request(request: Annotated[Any, Request]) -> Dict[str, str]:
 
 	return {"base_url": str(request.base_url), "path": request.url.path[1:]}
 
+
+@validate_call(config = ConfigDict(hide_input_in_errors=True))
+def calculate_start_pagination(page: int = PageDefault, pageSize:int = PageSizeDefault):
+	if page < PageDefault:
+		raise ValueError(f"El número de la página no es correcto [{page}]")
+
+	if pageSize < PageSizeLowerLimit or pageSize > PageSizeUpperLimit:
+		raise ValueError(f"El tamaño de la página no es el correcto [{pageSize}]")
+	
+	start = (page - 1) * pageSize
+
+	return start
