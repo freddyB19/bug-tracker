2325bd9f903453e8908db499b69c9d25954896b1
freddyB19 <freddyybolivar2013@gmail.com>
2025-05-06 00:00:16 -0400

    test: add func imports for tests/projects-commands

--- tests/projects/test_project_commands.py
+++ tests/projects/test_project_commands.py
@@ -6,42 +6,14 @@ from pydantic import ValidationError
 from tests import ENGINE
 from tests import SESSION
 from tests import get_db
+from tests.projects import set_project
+from tests.projects import set_project_schema
 
 from apps import Model
 from apps.users.models import User
 from apps.projects.models import Project
 from apps.projects.schemas import schemas
 from apps.projects.commands import commands
-from apps.projects.models import ChoicesPrority
-
-
-CHOICES = [choice.name for choice in ChoicesPrority]
-
-def set_project(title = None, description = None, priority = None, user_id = None):
-	set_title = title if title else "Proyecto de tests"
-	set_description = description if description else "Proyecto para hacer tests"
-	set_priority = priority if priority in CHOICES else ChoicesPrority.normal.name
-	set_user_id = user_id if user_id else 1
-
-	return Project(
-		title = set_title,
-		description = set_description,
-		priority = set_priority,
-		user_id = set_user_id
-	)
-
-def set_project_schema(title = None, description = None, priority = None, user_id = None):
-	set_title = title if title else "Proyecto de tests"
-	set_description = description if description else "Proyecto para hacer tests"
-	set_priority = priority if priority in CHOICES else ChoicesPrority.normal.name
-	set_user_id = user_id if user_id else 1
-
-	return schemas.ProjectRequest(
-		title = set_title,
-		description = set_description,
-		priority = set_priority,
-		user_id = set_user_id
-	)
 
 
 class TestCommandsProject:
